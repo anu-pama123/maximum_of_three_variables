92e55eefbad45e1f048b1ba7233530853319d719
anupama cv <anu.anupamacv@gmail.com>
2021-06-12 22:57:27 +0530

    REFACTORED: by generic class to find maximum value

--- src/main/java/com/bridgelabz/MaximumValueBuilder.java
+++ src/main/java/com/bridgelabz/MaximumValueBuilder.java
@@ -1,13 +1,27 @@
 package com.bridgelabz;
 
-public class MaximumValueBuilder {
+public class MaximumValueBuilder<E extends Comparable> {
+    E firstValue;
+    E secondValue;
+    E thirdValue;
 
-    public <E extends Comparable> E findMax(E firstValue, E secondValue, E thirdValue) {
-        E max =firstValue;
+    public MaximumValueBuilder(E firstValue, E secondValue, E thirdValue) {
+        this.firstValue = firstValue;
+        this.secondValue = secondValue;
+        this.thirdValue = thirdValue;
+    }
+
+    public E findMax(){
+        return MaximumValueBuilder.findMax(firstValue,secondValue,thirdValue);
+    }
+
+    public static <E extends Comparable> E findMax(E firstValue, E secondValue, E thirdValue) {
+        E max = firstValue;
         if (secondValue.compareTo(max)>0)
             max = secondValue;
         if (thirdValue.compareTo(max)>0)
             max = thirdValue;
         return max;
     }
+
 }
--- src/test/java/com/bridgelabz/MaximumValueTest.java
+++ src/test/java/com/bridgelabz/MaximumValueTest.java
@@ -6,73 +6,66 @@ import org.junit.Assert;
 public class MaximumValueTest {
 
     @Test
-    public void givenIntegerValue_whenCompared_returnsFirstValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(25,15,20);
-        Comparable expectedResult=25;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenIntegerValue_whenCompared_returnsSecondValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(15,25,20);
-        Comparable expectedResult=25;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenIntegerValue_whenCompared_returnsThirdValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(15,20,25);
-        Comparable expectedResult=25;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenFloatValue_whenCompared_returnsFirstValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(3.1f,2.3f,2.5f);
-        Comparable expectedResult=3.1f;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenFloatValue_whenCompared_returnsSecondValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(1.3f,4.2f,2.5f);
-        Comparable expectedResult=4.2f;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenFloatValue_whenCompared_returnsThirdValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax(1.8f,2.6f,3.5f);
-        Comparable expectedResult=3.5f;
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenStringValue_whenCompared_returnsFirstValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax("Peach","Apple","Banana");
-        Comparable expectedResult="Peach";
-        Assert.assertEquals(expectedResult,maxInteger);
-    }
-
-    @Test
-    public void givenStringValue_whenCompared_returnsSecondValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax("Apple","Peach","Banana");
-        Comparable expectedResult="Peach";
-        Assert.assertEquals(expectedResult,maxInteger);
+    public void givenIntegerValue_whenCompared_returnsFirstValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Integer>(25, 15, 5);
+        Comparable maxInteger = maximumValue.findMax();
+        Assert.assertEquals(25,maxInteger);
     }
     @Test
-    public void givenStringValue_whenCompared_returnsThirdValueAsMax() {
-        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
-        Comparable maxInteger = maximumValue.findMax("Apple","Banana","Peach");
-        Comparable expectedResult="Peach";
-        Assert.assertEquals(expectedResult,maxInteger);
+    public void givenIntegerValue_whenCompared_returnsSecondValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Integer>(5, 15, 10);
+        Comparable maxInteger = maximumValue.findMax();
+        Assert.assertEquals(15,maxInteger);
+    }
+    @Test
+    public void givenIntegerValue_whenCompared_returnsThirdValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Integer>(5, 15, 25);
+        Comparable maxInteger = maximumValue.findMax();
+        Assert.assertEquals(25,maxInteger);
+    }
+    @Test
+    public void givenFloatValue_whenCompared_returnsFirstValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Float>(25.5f, 15.2f, 5.7f);
+        Comparable maxFloat = maximumValue.findMax();
+        Assert.assertEquals(25.5f,maxFloat);
+    }
+    @Test
+    public void givenFloatValue_whenCompared_returnsSecondValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Float>(5.3f, 15.4f, 10.9f);
+        Comparable maxFloat = maximumValue.findMax();
+        Assert.assertEquals(15.4f,maxFloat);
+    }
+    @Test
+    public void givenFloatValue_whenCompared_returnsThirdValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<Float>(5.8f, 10.6f, 20.1f);
+        Comparable maxFloat = maximumValue.findMax();
+        Assert.assertEquals(20.1f,maxFloat);
+    }
+    @Test
+    public void givenStringValue_whenCompared_returnsFirstValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<String>("Orange", "Banana", "Apple");
+        Comparable maxString = maximumValue.findMax();
+        Assert.assertEquals("Orange", maxString);
+    }
+    @Test
+    public void givenStringValue_whenCompared_returnsSecondValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<String>("Banana", "Orange", "Apple");
+        Comparable maxString = maximumValue.findMax();
+        Assert.assertEquals("Orange", maxString);
+    }
+    @Test
+    public void givenStringValue_whenCompared_returnsThirdValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder<String>("Apple", "Banana", "Orange");
+        Comparable maxString = maximumValue.findMax();
+        Assert.assertEquals("Orange", maxString);
     }
 }
