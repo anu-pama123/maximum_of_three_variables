b24f13b5d6526c661a467223ff02c835a6be7b33
anupama cv <anu.anupamacv@gmail.com>
2021-06-12 19:03:10 +0530

    ADDED: functionality to find maximum of three integer values

--- src/main/java/com/bridgelabz/MaximumValueBuilder.java
+++ src/main/java/com/bridgelabz/MaximumValueBuilder.java
@@ -2,4 +2,13 @@ package com.bridgelabz;
 
 public class MaximumValueBuilder {
 
+    public Integer findMaximumValue(Integer firstNumber, Integer secondNumber, Integer thirdNumber) {
+        Integer max=firstNumber;
+        if (secondNumber.compareTo(max)>0)
+            max = secondNumber;
+        if (thirdNumber.compareTo(max)>0)
+            max = thirdNumber;
+        return max;
+    }
+
 }
--- src/test/java/com/bridgelabz/MaximumValueTest.java
+++ src/test/java/com/bridgelabz/MaximumValueTest.java
@@ -1,5 +1,28 @@
 package com.bridgelabz;
 
-public class MaximumValueTest {
+import org.junit.Test;
+import org.junit.Assert;
 
+public class MaximumValueTest {
+    @Test
+    public void givenIntegerValue_whenCompared_returnsFirstValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
+        int maxInteger = maximumValue.findMaximumValue(25, 15, 5);
+        Assert.assertEquals(25,maxInteger);
+    }
+    @Test
+    public void givenIntegerValue_whenCompared_returnsSecondValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
+        int maxInteger = maximumValue.findMaximumValue(5, 15, 10);
+        Assert.assertEquals(15,maxInteger);
+    }
+    @Test
+    public void givenIntegerValue_whenCompared_returnsThirdValueAsMax()
+    {
+        MaximumValueBuilder maximumValue = new MaximumValueBuilder();
+        int maxInteger = maximumValue.findMaximumValue(5, 10, 20);
+        Assert.assertEquals(20,maxInteger);
+    }
 }
